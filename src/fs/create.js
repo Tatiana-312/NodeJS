f9c9446fa4dd7d30f1fb87e000f4be72233f79ad
Tatiana-312 <tatyanapetrosh@gmail.com>
2022-12-01 16:55:02 +0600

    feat: finish fs create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { access, constants, writeFile } from 'node:fs/promises';
+
+const isFileExist = async (filePath) => {
+    try {
+        await access(filePath, constants.F_OK);
+        return true;
+    } catch {
+        return false;
+    }
+};
+
 const create = async () => {
-    // Write your code here 
+    try {
+        if (await isFileExist('./files/fresh.txt')) {
+            throw new Error;
+        }
+        await writeFile('./files/fresh.txt', 'I am fresh and young');
+    } catch {
+        console.error('FS operation failed');
+    }
 };
 
 await create();
