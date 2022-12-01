bee29cda42654a06aa61be23b838178e9931e8c7
Tatiana-312 <tatyanapetrosh@gmail.com>
2022-12-01 21:33:58 +0600

    refactor: add custom module

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -2,25 +2,17 @@ import { access, constants, cp } from "node:fs/promises";
 import { fileURLToPath } from "url";
 import path from "path";
 import { dirname } from "path";
+import { isExist } from "./customModules/exist.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-const isDirExist = async (dirPath) => {
-  try {
-    await access(dirPath, constants.F_OK);
-    return true;
-  } catch {
-    return false;
-  }
-};
 
 const copy = async () => {
   try {
     const srcPath = path.join(__dirname, "files");
     const destPath = path.join(__dirname, "files_copy");
 
-    if (await isDirExist(destPath)) {
+    if (await isExist(destPath)) {
       throw new Error("FS operation failed");
     }
     await cp(srcPath, destPath, { recursive: true });
--- src/fs/create.js
+++ src/fs/create.js
@@ -2,23 +2,15 @@ import { access, constants, writeFile } from "node:fs/promises";
 import { fileURLToPath } from "url";
 import path from "path";
 import { dirname } from "path";
+import { isExist } from "./customModules/exist.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const isFileExist = async (filePath) => {
-  try {
-    await access(filePath, constants.F_OK);
-    return true;
-  } catch {
-    return false;
-  }
-};
-
 const create = async () => {
   try {
     const filePath = path.join(__dirname, "files", "fresh.txt");
-    if (await isFileExist(filePath)) {
+    if (await isExist(filePath)) {
       throw new Error("FS operation failed");
     }
     await writeFile(filePath, "I am fresh and young");
