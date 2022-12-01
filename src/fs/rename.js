8c6e2e35ea578a2e4b3a613ac083c0b521981b88
Tatiana-312 <tatyanapetrosh@gmail.com>
2022-12-01 22:25:25 +0600

    refactor: refactor rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,15 +1,10 @@
 import fs from "fs/promises";
-import { fileURLToPath } from "url";
-import path from "path";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { getPath } from "./customModules/getPath.js";
 
 const rename = async () => {
   try {
-    const oldPath = path.join(__dirname, "files", "wrongFilename.txt");
-    const newPath = path.join(__dirname, "files", "properFilename.md");
+    const oldPath = getPath(import.meta.url, "files", "wrongFilename.txt");
+    const newPath = getPath(import.meta.url, "files", "properFilename.md");
     await fs.rename(oldPath, newPath);
   } catch {
     throw new Error("FS operation failed");
