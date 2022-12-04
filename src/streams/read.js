f157ded04619f3b572ae7cda6b0c4f2f765ed3c8
Tatiana-312 <tatyanapetrosh@gmail.com>
2022-12-04 17:35:22 +0600

    feat: finish stream read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'node:fs';
+import { getPath } from '../customModules/getPath.js';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const filePath = getPath(import.meta.url, 'files', 'fileToRead.txt');
+        const stream = createReadStream(filePath, 'utf-8');
+        stream.on('data', chunk => process.stdout.write(chunk));
+    } catch (err) {
+        throw err;
+    }
 };
 
 await read();
