efde1247cb34987f22d5c3438e3df421cc6c0fb1
Tatiana-312 <tatyanapetrosh@gmail.com>
2022-12-05 14:17:34 +0600

    feat: complete wt first task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,13 @@
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const worker = new Worker('./worker.js', {
+        workerData: 10
+    });
+
+    worker.on('message', msg => {
+        console.log(msg);
+    });
 };
 
 await performCalculations();
