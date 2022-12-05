4098641e02f70e1ca9bbf7c3ae24225de1d06393
Tatiana-312 <tatyanapetrosh@gmail.com>
2022-12-05 18:28:14 +0600

    feat: complete wt main task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,13 +1,30 @@
-import { Worker } from 'worker_threads';
+import { Worker } from "worker_threads";
+import os from "os";
 
 const performCalculations = async () => {
-    const worker = new Worker('./worker.js', {
-        workerData: 10
-    });
+  let initialNumber = 10;
+  const cpu = os.cpus();
 
-    worker.on('message', msg => {
-        console.log(msg);
+  const promises = cpu.map(() => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker("./worker.js", {
+        workerData: initialNumber++,
+      });
+      worker.on("message", (result) => resolve(result));
+      worker.on("error", (error) => reject(error));
     });
+  });
+
+  const arrOfWorkerResults = await Promise.allSettled(promises);
+
+  const result = arrOfWorkerResults.map(({ status, value }) => {
+    if (status === "fulfilled") {
+      return { status: "resolved", data: value };
+    } else {
+      return { status: "error", data: null };
+    }
+  });
+  console.log(result);
 };
 
 await performCalculations();
